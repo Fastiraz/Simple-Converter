ca64bcbf866e2de9f65852fd4d2387e537723d0a
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2023-03-08 17:16:57 +0100

    first commit

---
+++ converter/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace converter
+{
+    partial class converter
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(converter));
+            this.panldrop = new System.Windows.Forms.Panel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblfrom = new System.Windows.Forms.Label();
+            this.lblto = new System.Windows.Forms.Label();
+            this.tbxfrom = new System.Windows.Forms.TextBox();
+            this.tbxto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panldrop
+            // 
+            this.panldrop.AllowDrop = true;
+            this.panldrop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(46)))));
+            this.panldrop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panldrop.BackgroundImage")));
+            this.panldrop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panldrop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panldrop.Location = new System.Drawing.Point(0, 0);
+            this.panldrop.Name = "panldrop";
+            this.panldrop.Size = new System.Drawing.Size(624, 310);
+            this.panldrop.TabIndex = 0;
+            this.panldrop.DragDrop += new System.Windows.Forms.DragEventHandler(this.Drag_Drop);
+            this.panldrop.DragEnter += new System.Windows.Forms.DragEventHandler(this.Drag_Enter);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // lblfrom
+            // 
+            this.lblfrom.AutoSize = true;
+            this.lblfrom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(46)))));
+            this.lblfrom.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfrom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(68)))), ((int)(((byte)(55)))));
+            this.lblfrom.Location = new System.Drawing.Point(12, 9);
+            this.lblfrom.Name = "lblfrom";
+            this.lblfrom.Size = new System.Drawing.Size(44, 17);
+            this.lblfrom.TabIndex = 1;
+            this.lblfrom.Text = "From:";
+            // 
+            // lblto
+            // 
+            this.lblto.AutoSize = true;
+            this.lblto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(46)))));
+            this.lblto.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(68)))), ((int)(((byte)(55)))));
+            this.lblto.Location = new System.Drawing.Point(29, 39);
+            this.lblto.Name = "lblto";
+            this.lblto.Size = new System.Drawing.Size(27, 17);
+            this.lblto.TabIndex = 2;
+            this.lblto.Text = "To:";
+            // 
+            // tbxfrom
+            // 
+            this.tbxfrom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(46)))));
+            this.tbxfrom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxfrom.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxfrom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(68)))), ((int)(((byte)(55)))));
+            this.tbxfrom.Location = new System.Drawing.Point(62, 6);
+            this.tbxfrom.Name = "tbxfrom";
+            this.tbxfrom.Size = new System.Drawing.Size(100, 22);
+            this.tbxfrom.TabIndex = 3;
+            this.tbxfrom.Text = ".docx";
+            this.tbxfrom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbxto
+            // 
+            this.tbxto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(46)))));
+            this.tbxto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxto.Enabled = false;
+            this.tbxto.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(68)))), ((int)(((byte)(55)))));
+            this.tbxto.Location = new System.Drawing.Point(62, 39);
+            this.tbxto.Name = "tbxto";
+            this.tbxto.Size = new System.Drawing.Size(100, 22);
+            this.tbxto.TabIndex = 4;
+            this.tbxto.Text = ".pdf";
+            this.tbxto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // converter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(624, 310);
+            this.Controls.Add(this.tbxto);
+            this.Controls.Add(this.tbxfrom);
+            this.Controls.Add(this.lblto);
+            this.Controls.Add(this.lblfrom);
+            this.Controls.Add(this.panldrop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "converter";
+            this.Text = "Fastiraz";
+            this.Load += new System.EventHandler(this.converter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panldrop;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblfrom;
+        private System.Windows.Forms.Label lblto;
+        private System.Windows.Forms.TextBox tbxfrom;
+        private System.Windows.Forms.TextBox tbxto;
+    }
+}
+
---
+++ converter/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace converter
+{
+    public partial class converter : Form
+    {
+        public converter()
+        {
+            InitializeComponent();
+        }
+
+        private void converter_Load(object sender, EventArgs e)
+        {
+            tbxfrom.Enabled = true;
+            tbxto.Enabled = false;
+        }
+
+        private void Drag_Enter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        BackgroundWorker bw;
+
+        private void Drag_Drop(object sender, DragEventArgs e)
+        {
+            bw = new BackgroundWorker();
+            bw.DoWork += (obj, ea) => TaskAsync(1, e);
+            bw.RunWorkerAsync();
+        }
+
+        private async void TaskAsync(int times, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            for (int i = 0; i < s.Length; i++)
+            {
+                //MessageBox.Show(s[i]);
+                DirectoryInfo d = new DirectoryInfo(s[i]);
+                FileInfo[] files = d.GetFiles($"*{tbxfrom.Text}");
+                foreach (FileInfo fi in files)
+                {
+                    //string file = Convert.ToString(fi.FullName);
+                    string destfile = fi.FullName + tbxto.Text;
+                    //MessageBox.Show($"File: {fi.FullName}\nDest: {destfile}");
+                    if (tbxfrom.Text == ".docx")
+                    {
+                        docx2pdf(fi.FullName, destfile, WdSaveFormat.wdFormatPDF);
+                    }
+                    else if (tbxfrom.Text == ".xlsx")
+                    {
+                        excel2pdf(fi.FullName, destfile);
+                    }
+                    else if (tbxfrom.Text == ".pptx")
+                    {
+                        ppt2pdf(fi.FullName, destfile);
+                    }
+
+                    //Console.ReadKey();
+                }
+            }
+            notifyIcon1.Icon = SystemIcons.Shield;
+            notifyIcon1.BalloonTipTitle = "Notification";
+            notifyIcon1.BalloonTipText = "Convertion complete...";
+            notifyIcon1.ShowBalloonTip(10000);
+        }
+
+        public static void docx2pdf(string input, string output, WdSaveFormat format)
+        {
+            // Create an instance of Word.exe
+            Microsoft.Office.Interop.Word._Application oWord = new Word.Application
+            {
+
+                // Make this instance of word invisible (Can still see it in the taskmgr).
+                Visible = false
+            };
+
+            // Interop requires objects.
+            object oMissing = System.Reflection.Missing.Value;
+            object isVisible = true;
+            object readOnly = true;     // Does not cause any word dialog to show up
+            //object readOnly = false;  // Causes a word object dialog to show at the end of the conversion
+            object oInput = input;
+            object oOutput = output;
+            object oFormat = format;
+
+            // Load a document into our instance of word.exe
+            _Document oDoc = oWord.Documents.Open(
+                ref oInput, ref oMissing, ref readOnly, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref isVisible, ref oMissing, ref oMissing, ref oMissing, ref oMissing
+                );
+
+            // Make this document the active document.
+            oDoc.Activate();
+
+            // Save this document using Word
+            oDoc.SaveAs(ref oOutput, ref oFormat, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing
+                );
+
+            // Always close Word.exe.
+            oWord.Quit(ref oMissing, ref oMissing, ref oMissing);
+        }
+
+        public static void excel2pdf(string input, string output)
+        {
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            Workbook excelWorkbook = excelApp.Workbooks.Open(input);
+            excelWorkbook.ExportAsFixedFormat(Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF, output);
+            excelWorkbook.Close();
+            excelApp.Quit();
+        }
+
+        public static void ppt2pdf(string input, string output)
+        {
+            Microsoft.Office.Interop.PowerPoint.Application pptApp = new Microsoft.Office.Interop.PowerPoint.Application();
+            Microsoft.Office.Interop.PowerPoint.Presentation pptPresentation = pptApp.Presentations.Open(input);
+            pptPresentation.ExportAsFixedFormat(output, PpFixedFormatType.ppFixedFormatTypePDF);
+            pptPresentation.Close();
+            pptApp.Quit();
+        }
+    }
+}
